263d619ece6f428073ee2b883d65a1b932cdd5f3
Anthony Maina <anthony.maina@savana.co.ke>
2018-07-15 21:21:27 -0400

    Travis

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+  mongoURI: 'mongodb://test:password1@ds127781.mlab.com:27781/node_advanced',
+  cookieKey: '123123123',
+    redisUrl:'redis://127.0.0.1:6379'
 };
